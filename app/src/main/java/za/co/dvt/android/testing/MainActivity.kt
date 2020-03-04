b44797b78ae3b73e262bf03bab4b399720d6ff68
Peter-John Welcome <pjwelcome@gmail.com>
2020-03-04 14:32:35 +0200

    Rx Example for Tests
    Created a ViewModel that fetches a list of plants
    Create a Repo that fetched the plants from its data source
    Added Unit tests for the ViewModel

--- app/src/main/java/za/co/dvt/android/testing/MainActivity.kt
+++ app/src/main/java/za/co/dvt/android/testing/MainActivity.kt
@@ -1,5 +1,66 @@
 package za.co.dvt.android.testing
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) , TheView {
+
+    private val disposable = CompositeDisposable()
+
+    private val viewModel: RxViewModel by lazy {
+        return@lazy RxViewModel(this, RepositoryImpl())
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fetchPlantList()
+    }
+
+    private fun fetchPlantList() {
+        disposable.add(
+                viewModel.fetchPlants().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe {
+                    print(it.map { plant -> plant.name })
+                })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable.dispose()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        disposable.clear()
+    }
+
+    override fun showSuccessfulMessage() {
+        print("Yaye")
+    }
+}
+
+interface TheView {
+    fun showSuccessfulMessage()
+}
+
+class RxViewModel (private  val view: TheView, private val repo: Repository ){
+
+    fun fetchPlants() : Flowable<List<Plant>> {
+        view.showSuccessfulMessage()
+        return repo.getPlants().map { it.filter { plant ->  plant.name == "Tree" } }
+    }
+}
+
+interface Repository  {
+    fun getPlants(): Flowable<List<Plant>>
+}
+
+class RepositoryImpl : Repository  {
+    override fun getPlants(): Flowable<List<Plant>> = Flowable.just(listOf(Plant("Tree", "Green")))
+}
+
+data class Plant (var name : String, var color : String = "Green")
